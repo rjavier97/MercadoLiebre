d43180446584c2b9505cfc3dc8b7e3b5d81429b3
rjavier97 <rjavier.suarez@hotmail.com>
2023-02-08 10:54:16 -0300

    se modifico pagina principal

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const publicPath = path.join(__dirname, '/public');
 
 app.use( express.static(publicPath) );
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
