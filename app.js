16f462186f6bfef6bb4cfd3e1d9bd3c27fa11748
rjavier97 <rjavier.suarez@hotmail.com>
2023-02-21 21:20:22 -0300

    modificaciones

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require('path');
 const publicPath = path.join(__dirname, '/public');
 
 app.use( express.static(publicPath) );
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 });
